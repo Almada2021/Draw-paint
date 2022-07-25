d1c5f46afe8d820f24371545eb7901195089782d
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2022-07-25 07:14:29 +0000

    first version simplify

---
+++ drawpoint/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Canvas from './components/Canvas';
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas width={"800"} height={"450"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ drawpoint/src/components/Hooks.js
@@ -0,0 +1,75 @@
+import {useRef, useEffect} from "react"
+export function useOnDraw(onDraw){
+    const canvasRef = useRef(null);
+    const isDrawingRef = useRef(false)
+    const mouseMoveListenerRef = useRef(null);
+    const mouseDownListenerRef = useRef(null);
+    const mouseUpListenerRef = useRef(null);
+    const prevPointRef =  useRef(null);
+
+    useEffect(() => {
+        return () => {
+            if(mouseMoveListenerRef.current){
+                window.removeEventListener("mousemove", mouseMoveListenerRef);
+            }
+            if(mouseUpListenerRef.current){
+                window.removeEventListener("mouseup", mouseUpListenerRef)
+            }
+        };
+    }, []);
+    function setCanvasRef(ref){
+        if(!ref) return;
+        if(canvasRef.current){
+            canvasRef.current.removeEventListener("mousedown",mouseDownListenerRef.current);
+        }
+        canvasRef.current = ref;
+        initMouseMoveListener();
+        initMouseDownListener();
+        initMouseUpListener();
+    }
+    function initMouseMoveListener(){
+        const mouseMoveListener = (e) =>{
+            if(isDrawingRef.current){
+                const point =computePounteInCanvas(e.clientX, e.clientY);
+                const ctx = canvasRef.current.getContext('2d');
+                if(onDraw) onDraw(ctx, point, prevPointRef.current);
+                prevPointRef.current = point;
+            }
+
+        } 
+        mouseMoveListenerRef.current = mouseMoveListener 
+        window.addEventListener("mousemove", mouseMoveListener)
+    }
+    function initMouseUpListener(){
+        const listener = () =>{
+            isDrawingRef.current = false;
+            prevPointRef.current = null;
+        }  
+        mouseUpListenerRef.current = listener; 
+
+        window.addEventListener("mouseup", listener);
+    }
+    function initMouseDownListener(){
+        if(!canvasRef.current) return ;
+        const listener = () =>{
+            isDrawingRef.current = true;
+
+        }
+        mouseDownListenerRef.current = listener; 
+        canvasRef.current.addEventListener("mousedown", listener)
+    }
+    function computePounteInCanvas(clientX,clientY){
+        if(canvasRef.current){
+
+            const boudingRect = canvasRef.current.getBoundingClientRect();
+             return {
+                x : clientX - boudingRect.left,
+                y : clientY - boudingRect.top
+            }
+        }else {
+            return null
+        }
+
+    } 
+    return setCanvasRef;
+}
